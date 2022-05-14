ec7ed2783756f9eebb3301f0281218e1a64a25e3
minirom <romainleboudec@gmail.com>
2022-05-14 19:28:05 +0200

    Revert "core- remove useless file"
    This reverts commit 75eb18c9c6c7e4937da9a21ea204c0046b41e171.

---
+++ src/enums/SportsEnum.js
@@ -0,0 +1,9 @@
+export default Object.freeze({
+	BASEBALL: 'baseball',
+	BASKETBALL: 'basketball',
+	FOOTBALL: 'american-football',
+	HANDBALL: 'handball',
+	HOCKEY: 'hockey',
+	RUGBY: 'rugby',
+	SOCCER: 'soccer',
+});
