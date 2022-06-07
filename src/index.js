fbee05a31e7f79b3bebe3bde20cf6998050a3af0
minirom <romainleboudec@gmail.com>
2022-06-07 19:35:47 +0200

    core: add enum for forfeit winner

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as SportsEnum } from './enums/SportsEnum.js';
+export { default as ForfeitWinnerEnum } from './enums/ForfeitWinnerEnum.js';
+
 export { default as Logger } from './utils/Logger.js';
