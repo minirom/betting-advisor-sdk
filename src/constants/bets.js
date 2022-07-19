8d0383fc5f77b5fe9c9fe6e7e028280080be9f75
minirom <romainleboudec@gmail.com>
2022-07-19 23:00:09 +0200

    core: add bets constants

---
+++ src/constants/bets.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const START_BETS = 2017;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export { default as SportsEnum } from './enums/SportsEnum.js';
 export { default as ForfeitWinnerEnum } from './enums/ForfeitWinnerEnum.js';
 
 export { default as Logger } from './utils/Logger.js';
+
+export * from './constants/bets.js';
