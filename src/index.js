338b3954be3844e8194d096afbce9592ceeaa197
minirom <romainleboudec@gmail.com>
2022-08-24 10:55:35 +0200

    core: export enums

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-export { default as SportsEnum } from './enums/SportsEnum.js';
 export { default as ForfeitWinnerEnum } from './enums/ForfeitWinnerEnum.js';
 export { default as RecordTypeEnum } from './enums/RecordTypeEnum.js';
+export { default as SportsEnum } from './enums/SportsEnum.js';
 export { default as StatisticsPeriodEnum } from './enums/StatisticsPeriodEnum.js';
+export { default as StrategyTypeEnum } from './enums/StrategyTypeEnum.js';
+export { default as TopEnum } from './enums/TopEnum.js';
 
 export { default as Logger } from './utils/Logger.js';
 
